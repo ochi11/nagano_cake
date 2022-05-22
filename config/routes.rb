c332ae772b4fe59458456ad94577d3d6d90152ed
EC2 Default User <ec2-user@ip-172-31-27-167.ec2.internal>
2022-05-22 12:27:00 +0000

    [Start]migrationファイルにカラムの追加から

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+  get '/' => 'homes#top'
+
+  namespace :admin do
+    resources :items
+    resources :homes
+    resources :genres
+    resources :customers
+    resources :orders
+    resources :order_details
+  end
+
+  resources :homes
+  resources :items
+  resources :customers
+  resources :cart_items
+  resources :orders
+  resources :addresses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
